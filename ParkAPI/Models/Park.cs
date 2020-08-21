968de88bd3f621f14bc9fbb2551c0c93a0a47c02
Joseph Pearce <joseph.pearce05@gmail.com>
2020-08-21 09:42:19 -0700

    add Model, troubleshoot database migration

---
+++ ParkAPI/Models/Park.cs
@@ -0,0 +1,11 @@
+namespace ParkAPI.Models
+{
+    public class Park
+    {
+        public int ParkId { get; set; }
+        public string ParkName { get; set; }
+        public int Rating {get;set;}
+        public string Description {get;set;}
+        public string State {get;set;}
+    }
+}
--- ParkAPI/Startup.cs
+++ ParkAPI/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
-using ParkAPI.Controllers;
+using ParkAPI.Models;
 
 
 namespace ParkAPI
